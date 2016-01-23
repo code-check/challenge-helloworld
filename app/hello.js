cdc2f793c438da5ddf2dd251c4005aeaea20aab8
akihiro nitta <dustbox.1218@gmail.com>
2016-01-23 17:36:07 +0900

    Update hello.js

--- app/hello.js
+++ app/hello.js
@@ -3,7 +3,7 @@ function hello(name) {
 * ここにnameに"World"が入力された時、この関数が"Hello World!"と返すような実装をします。
 * nameが"codecheck"の場合には、"Hello codecheck!"が返されます。
 */
-
+  //ここにHelloworldを実装します。
   //return "Hello World!";
   //BEGIN_CHALLENGE
   return "Hello "+name+"!";
