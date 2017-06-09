a742d6d8e2881f56d3c5c1e69e8bfbaa47b96e52
TsubasaK111 <Tsubasa.K111@gmail.com>
2017-06-09 14:40:43 +0900

    feat: add skeleton template files

--- test/basic.js
+++ test/basic.js
@@ -1,5 +1,6 @@
 var assert = require("chai").assert;
 var hello  = require("../app/hello.js");
+
 describe("helloWorld", function() {
   it("Hello World!", function() {
     assert.equal(hello("World"), "Hello World!");
