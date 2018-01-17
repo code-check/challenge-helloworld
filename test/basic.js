9b2891d5d21015b974559771a7be83542c19713b
TsubasaK111 <Tsubasa.K111@gmail.com>
2018-01-17 15:50:48 +0900

    feat: translate and update test cases

--- test/basic.js
+++ test/basic.js
@@ -4,31 +4,24 @@ const expect = require("chai").expect;
 const codecheck = require("codecheck");
 const app = codecheck.consoleApp(process.env.APP_COMMAND);
 
-describe("CLIアプリは", () => {
-  it("'World'が与えられた場合に'Hello World!'と出力できる。", () => {
+describe("CLI App", () => {
+  it("outputs 'Hello World!' when given 'World' as an argument", () => {
   	return app.codecheck("World").then( result =>  {
-      expect(result.code).to.equal(0, "expect codecheck CLI to exit with status code 0");
+      expect(result.code).to.equal(0, "expected codecheck CLI to exit with status code 0");
       expect(result.stdout.join("")).to.equal("Hello World!");
   	})
   });
 
-  it("ゼロ長の文字列を与えられた場合に'Hello!'と出力できる。", () => {
-  	return app.codecheck("").then( result =>  {
-      expect(result.code).to.equal(0, "expect codecheck CLI to exit with status code 0");
-      expect(result.stdout.join("")).to.equal("Hello!");
-  	})
-  });
-
-  it("'codecheck'が与えられた場合に'Hello codecheck!'と出力できる。", () => {
+  it("outputs 'Hello codecheck!' when given 'codecheck' as an argument.", () => {
   	return app.codecheck("codecheck").then( result =>  {
-      expect(result.code).to.equal(0, "expect codecheck CLI to exit with status code 0");
+      expect(result.code).to.equal(0, "expected codecheck CLI to exit with status code 0");
       expect(result.stdout.join("")).to.equal("Hello codecheck!");
   	})
   });
 
-  it("'織田信長'が与えられた場合に'Hello 織田信長!'と出力できる。", () => {
+  it("outputs 'Hello 織田信長!' when given '織田信長' as an argument.", () => {
   	return app.codecheck("織田信長").then( result =>  {
-      expect(result.code).to.equal(0, "expect codecheck CLI to exit with status code 0");
+      expect(result.code).to.equal(0, "expected codecheck CLI to exit with status code 0");
       expect(result.stdout.join("")).to.equal("Hello 織田信長!");
   	})
   });
